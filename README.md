03b55771976b4e7abde3a8298db7cf9ebb36fcd1
RookieRoad <ssl2001@126.com>
2023-10-30 21:30:08 +0800

    add vcpkg config to project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# mlk-graph-slam
-Create a slam project, which uses multi-layer optical flow method as the front end and graph optimization as the back end.
+# MLK-SLAM
+
+![C++](https://img.shields.io/badge/c++-std14-blue)
+![ubuntu](https://img.shields.io/badge/platform-ubuntu20.04-orange)
+![SLAM](https://img.shields.io/badge/slam-stereo--visual-green)
+![LK](https://img.shields.io/badge/opticalFlow-LK-brightgreen)
+![BA](https://img.shields.io/badge/BA-SildingWindow-yellowgreen)
+![License](https://img.shields.io/badge/license-Apache-blue)
+![version](https://img.shields.io/badge/version-v1.0.0-orange)
+
+
+该项目构建了，一种基于多层光流法的前端和滑动窗口后端BA的简单slam系统。
+
+https://www.youtube.com/watch?v=6GFBdjZPgTg
--- cmake/FindG2O.cmake
+++ cmake/FindG2O.cmake
@@ -1,113 +1,155 @@
-# Find the header files
-
-FIND_PATH(G2O_INCLUDE_DIR g2o/core/base_vertex.h
-  ${G2O_ROOT}/include
-  $ENV{G2O_ROOT}/include
-  $ENV{G2O_ROOT}
-  /usr/local/include
-  /usr/include
-  /opt/local/include
-  /sw/local/include
-  /sw/include
-  NO_DEFAULT_PATH
-  )
-
-# Macro to unify finding both the debug and release versions of the
-# libraries; this is adapted from the OpenSceneGraph FIND_LIBRARY
-# macro.
-
-MACRO(FIND_G2O_LIBRARY MYLIBRARY MYLIBRARYNAME)
-
-  FIND_LIBRARY("${MYLIBRARY}_DEBUG"
-    NAMES "g2o_${MYLIBRARYNAME}_d"
-    PATHS
-    ${G2O_ROOT}/lib/Debug
-    ${G2O_ROOT}/lib
-    $ENV{G2O_ROOT}/lib/Debug
-    $ENV{G2O_ROOT}/lib
-    NO_DEFAULT_PATH
-    )
-
-  FIND_LIBRARY("${MYLIBRARY}_DEBUG"
-    NAMES "g2o_${MYLIBRARYNAME}_d"
-    PATHS
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/local/lib64
-    /usr/lib
-    /usr/lib64
-    /opt/local/lib
-    /sw/local/lib
-    /sw/lib
-    )
-  
-  FIND_LIBRARY(${MYLIBRARY}
-    NAMES "g2o_${MYLIBRARYNAME}"
-    PATHS
-    ${G2O_ROOT}/lib/Release
-    ${G2O_ROOT}/lib
-    $ENV{G2O_ROOT}/lib/Release
-    $ENV{G2O_ROOT}/lib
-    NO_DEFAULT_PATH
-    )
-
-  FIND_LIBRARY(${MYLIBRARY}
-    NAMES "g2o_${MYLIBRARYNAME}"
-    PATHS
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/lib
-    /usr/local/lib64
-    /usr/lib
-    /usr/lib64
-    /opt/local/lib
-    /sw/local/lib
-    /sw/lib
-    )
-  
-  IF(NOT ${MYLIBRARY}_DEBUG)
-    IF(MYLIBRARY)
-      SET(${MYLIBRARY}_DEBUG ${MYLIBRARY})
-    ENDIF(MYLIBRARY)
-  ENDIF( NOT ${MYLIBRARY}_DEBUG)
-  
-ENDMACRO(FIND_G2O_LIBRARY LIBRARY LIBRARYNAME)
+find_path(G2O_INCLUDE_DIRS g2o/core/base_vertex.h
+          "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/include"
+          "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/include")
 
 # Find the core elements
-FIND_G2O_LIBRARY(G2O_STUFF_LIBRARY stuff)
-FIND_G2O_LIBRARY(G2O_CORE_LIBRARY core)
+find_library(
+        G2O_STUFF_LIBRARY
+        NAMES g2o_stuff
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+
+find_library(
+        G2O_CORE_LIBRARY
+        NAMES g2o_core
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
 
 # Find the CLI library
-FIND_G2O_LIBRARY(G2O_CLI_LIBRARY cli)
+find_library(
+        G2O_CLI_LIBRARY
+        NAMES g2o_cli
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
 
 # Find the pluggable solvers
-FIND_G2O_LIBRARY(G2O_SOLVER_CHOLMOD solver_cholmod)
-FIND_G2O_LIBRARY(G2O_SOLVER_CSPARSE solver_csparse)
-FIND_G2O_LIBRARY(G2O_SOLVER_CSPARSE_EXTENSION csparse_extension)
-FIND_G2O_LIBRARY(G2O_SOLVER_DENSE solver_dense)
-FIND_G2O_LIBRARY(G2O_SOLVER_PCG solver_pcg)
-FIND_G2O_LIBRARY(G2O_SOLVER_SLAM2D_LINEAR solver_slam2d_linear)
-FIND_G2O_LIBRARY(G2O_SOLVER_STRUCTURE_ONLY solver_structure_only)
-FIND_G2O_LIBRARY(G2O_SOLVER_EIGEN solver_eigen)
+find_library(
+        G2O_SOLVER_CHOLMOD
+        NAMES g2o_solver_cholmod
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_CSPARSE
+        NAMES g2o_solver_csparse
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_CSPARSE_EXTENSION
+        NAMES g2o_csparse_extension
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_DENSE
+        NAMES g2o_solver_dense
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_PCG
+        NAMES g2o_solver_pcg
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_SLAM2D_LINEAR
+        NAMES g2o_solver_slam2d_linear
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_STRUCTURE_ONLY
+        NAMES g2o_solver_structure_only
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_SOLVER_EIGEN
+        NAMES g2o_solver_eigen
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
 
 # Find the predefined types
-FIND_G2O_LIBRARY(G2O_TYPES_DATA types_data)
-FIND_G2O_LIBRARY(G2O_TYPES_ICP types_icp)
-FIND_G2O_LIBRARY(G2O_TYPES_SBA types_sba)
-FIND_G2O_LIBRARY(G2O_TYPES_SCLAM2D types_sclam2d)
-FIND_G2O_LIBRARY(G2O_TYPES_SIM3 types_sim3)
-FIND_G2O_LIBRARY(G2O_TYPES_SLAM2D types_slam2d)
-FIND_G2O_LIBRARY(G2O_TYPES_SLAM3D types_slam3d)
+find_library(
+        G2O_TYPES_DATA
+        NAMES g2o_types_data
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_ICP
+        NAMES g2o_types_icp
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_SBA
+        NAMES g2o_types_sba
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_SCLAM2D
+        NAMES g2o_types_sclam2d
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_SIM3
+        NAMES g2o_types_sim3
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_SLAM2D
+        NAMES g2o_types_slam2d
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
+find_library(
+        G2O_TYPES_SLAM3D
+        NAMES g2o_types_slam3d
+        PATHS "$ENV{VCPKG_ROOT}/installed/${PLATFORM}/lib"
+              "${PROJECT_BINARY_DIR}/vcpkg_installed/${PLATFORM}/lib")
 
-# G2O solvers declared found if we found at least one solver
-SET(G2O_SOLVERS_FOUND "NO")
-IF(G2O_SOLVER_CHOLMOD OR G2O_SOLVER_CSPARSE OR G2O_SOLVER_DENSE OR G2O_SOLVER_PCG OR G2O_SOLVER_SLAM2D_LINEAR OR G2O_SOLVER_STRUCTURE_ONLY OR G2O_SOLVER_EIGEN)
-  SET(G2O_SOLVERS_FOUND "YES")
-ENDIF(G2O_SOLVER_CHOLMOD OR G2O_SOLVER_CSPARSE OR G2O_SOLVER_DENSE OR G2O_SOLVER_PCG OR G2O_SOLVER_SLAM2D_LINEAR OR G2O_SOLVER_STRUCTURE_ONLY OR G2O_SOLVER_EIGEN)
+set(G2O_SOLVERS_FOUND "NO")
+if(G2O_SOLVER_CHOLMOD
+   OR G2O_SOLVER_CSPARSE
+   OR G2O_SOLVER_DENSE
+   OR G2O_SOLVER_PCG
+   OR G2O_SOLVER_SLAM2D_LINEAR
+   OR G2O_SOLVER_STRUCTURE_ONLY
+   OR G2O_SOLVER_EIGEN)
+        set(G2O_SOLVERS_FOUND "YES")
+endif(
+        G2O_SOLVER_CHOLMOD
+        OR G2O_SOLVER_CSPARSE
+        OR G2O_SOLVER_DENSE
+        OR G2O_SOLVER_PCG
+        OR G2O_SOLVER_SLAM2D_LINEAR
+        OR G2O_SOLVER_STRUCTURE_ONLY
+        OR G2O_SOLVER_EIGEN)
 
-# G2O itself declared found if we found the core libraries and at least one solver
-SET(G2O_FOUND "NO")
-IF(G2O_STUFF_LIBRARY AND G2O_CORE_LIBRARY AND G2O_INCLUDE_DIR AND G2O_SOLVERS_FOUND)
-  SET(G2O_FOUND "YES")
-ENDIF(G2O_STUFF_LIBRARY AND G2O_CORE_LIBRARY AND G2O_INCLUDE_DIR AND G2O_SOLVERS_FOUND)
+set(G2O_FOUND "NO")
+if(G2O_STUFF_LIBRARY
+   AND G2O_CORE_LIBRARY
+   AND G2O_INCLUDE_DIRS
+   AND G2O_SOLVERS_FOUND
+   AND PLATFORM)
+        set(G2O_FOUND "YES")
+endif(
+        G2O_STUFF_LIBRARY
+        AND G2O_CORE_LIBRARY
+        AND G2O_INCLUDE_DIRS
+        AND G2O_SOLVERS_FOUND
+        AND PLATFORM)
+if(G2O_FOUND AND G2O_SOLVERS_FOUND)
+        set(G2O_LIBRARIES
+            ${G2O_CORE_LIBRARY}
+            ${G2O_STUFF_LIBRARY}
+            ${G2O_CLI_LIBRARY}
+            ${G2O_SOLVER_CHOLMOD}
+            ${G2O_SOLVER_CSPARSE}
+            ${G2O_SOLVER_CSPARSE_EXTENSION}
+            ${G2O_SOLVER_DENSE}
+            ${G2O_SOLVER_PCG}
+            ${G2O_SOLVER_SLAM2D_LINEAR}
+            ${G2O_SOLVER_STRUCTURE_ONLY}
+            ${G2O_SOLVER_EIGEN}
+            ${G2O_TYPES_DATA}
+            ${G2O_TYPES_ICP}
+            ${G2O_TYPES_SBA}
+            ${G2O_TYPES_SCLAM2D}
+            ${G2O_TYPES_SIM3}
+            ${G2O_TYPES_SLAM2D}
+            ${G2O_TYPES_SLAM3D})
+endif(G2O_FOUND AND G2O_SOLVERS_FOUND)
